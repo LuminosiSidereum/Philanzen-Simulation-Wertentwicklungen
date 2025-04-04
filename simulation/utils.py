82f760a80cff96d0ffc632325934ec110890cc2a
Schulti <phillipp-schultheiss@web.de>
2025-04-04 18:16:40 +0200

    refactor: Formated the code base in utils.py

--- simulation/utils.py
+++ simulation/utils.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import logging
 import json
-import pandas as pd
+import pandas as pd  # type: ignore
 from pandas import DataFrame
 
 # Variables
@@ -9,10 +9,11 @@ global_variables: dict = {"root_path": None}
 
 # Initalize base logic for the libary
 logger = logging.getLogger(__name__)
-global_variables['root_path'] = Path(__file__).parent.parent
+global_variables["root_path"] = Path(__file__).parent.parent
 
-#MARK: Functions
-def load_text_json(*,language: str, interface: str, filename : str)-> dict:
+
+# MARK: Functions
+def load_text_json(*, language: str, interface: str, filename: str) -> dict:
     """
     Load text from a JSON file based on the specified language and interface.
 
@@ -25,8 +26,10 @@ def load_text_json(*,language: str, interface: str, filename : str)-> dict:
         dict: A dictionary containing the text for the specified interface in the proper language.
     """
     try:
-        file_path = Path(global_variables["root_path"])/ "resources" / f"{filename}.json"
-        with open(file_path, 'r', encoding='utf-8') as file:
+        file_path = (
+            Path(global_variables["root_path"]) / "resources" / f"{filename}.json"
+        )
+        with open(file_path, "r", encoding="utf-8") as file:
             text = json.load(file)
         return text[language][interface]
     except FileNotFoundError:
@@ -35,7 +38,8 @@ def load_text_json(*,language: str, interface: str, filename : str)-> dict:
     except json.JSONDecodeError:
         logger.error(f"Error decoding JSON from file: {file_path}")
         return {}
-    
+
+
 def load_settings() -> dict:
     """
     Load settings from a JSON file.
@@ -44,8 +48,8 @@ def load_settings() -> dict:
         dict: A dictionary containing the settings.
     """
     try:
-        file_path = global_variables["root_path"]/"resources"/"settings.json"
-        with open(file_path, 'r', encoding='utf-8') as file:
+        file_path = global_variables["root_path"] / "resources" / "settings.json"
+        with open(file_path, "r", encoding="utf-8") as file:
             settings = json.load(file)
         return settings
     except FileNotFoundError:
@@ -54,7 +58,8 @@ def load_settings() -> dict:
     except json.JSONDecodeError:
         logger.error(f"Error decoding JSON from file: {file_path}")
         return {}
-    
+
+
 def user_input_float(ui_text_input_request: str, ui_text_invalid_input: str) -> float:
     """
     Get a float input from the user with a prompt.
@@ -73,6 +78,7 @@ def user_input_float(ui_text_input_request: str, ui_text_invalid_input: str) -> 
         except ValueError:
             print(ui_text_invalid_input)
 
+
 def user_input_int(ui_text_input_request: str, ui_text_invalid_input: str) -> int:
     """
     Get an int input from the user with a prompt.
@@ -90,7 +96,8 @@ def user_input_int(ui_text_input_request: str, ui_text_invalid_input: str) -> in
             return user_input
         except ValueError:
             print(ui_text_invalid_input)
-            
+
+
 def save_dataframe_to_csv(df: DataFrame, filename: str) -> None:
     """
     Save a DataFrame to a CSV file.
@@ -100,7 +107,9 @@ def save_dataframe_to_csv(df: DataFrame, filename: str) -> None:
         filename (str): The name of the file to save the DataFrame to.
     """
     try:
-        file_path = global_variables["root_path"]/"data"/"output"/f"{filename}.csv"
+        file_path = (
+            global_variables["root_path"] / "data" / "output" / f"{filename}.csv"
+        )
         df.to_csv(file_path, index=False)
         logger.info(f"DataFrame saved to {file_path}")
     except Exception as e:
